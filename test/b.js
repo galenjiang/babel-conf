b5e1747f08df72de2b92dd7c1abfe36f40c124fd
galenjiang <249254832@qq.com>
2017-02-03 17:42:39 +0800

    refact config

--- test/b.js
+++ test/b.js
@@ -1,14 +1,20 @@
 "use strict";
 
-var _from = require("babel-runtime/core-js/array/from");
+var _loop = function _loop(i) {
+  setTimeout(function () {
+    return console.log(i);
+  });
+};
 
-var _from2 = _interopRequireDefault(_from);
+// import 'babel-polyfill';
+// var newArr = Array.from({
+//     1: "a",
+//     2: "b",
+//     3: "c",
+//     length: 3
+// })
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var newArr = (0, _from2.default)({
-    1: "a",
-    2: "b",
-    3: "c",
-    length: 3
-});
+for (var i = 0; i < 5; i++) {
+  _loop(i);
+}
